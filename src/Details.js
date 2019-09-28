cb40d4f651c3efc9589dcc481a1bd481b9bd4d70
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-09-28 00:29:15 -0500

    adds reach-router and heading takes you home

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Details = () => {
-  return <h1>hi lol </h1>;
-};
+const Details = props => (
+  <pre>
+    <code>{JSON.stringify(props, null, 4)}</code>
+  </pre>
+);
 
 export default Details;
