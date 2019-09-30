e4b950eae0687885920bc6ab127911481e4fc10b
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-09-29 23:39:42 -0500

    adds portal to handle modals

--- src/Details.js
+++ src/Details.js
@@ -1,15 +1,18 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends React.Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   componentDidMount() {
-    throw new Error("lol");
     pet.animal(+this.props.id).then(({ animal }) => {
       this.setState({
+        url: animal.url,
         name: animal.name,
         animal: animal.type,
         location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
@@ -21,12 +24,25 @@ class Details extends React.Component {
     }, console.error);
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  adopt = () => navigate(this.state.url);
+
   render() {
     if (this.state.loading) {
       return <h1>loading...</h1>;
     }
 
-    const { animal, breed, location, description, name, media } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      url,
+      media,
+      showModal
+    } = this.state;
 
     return (
       <div className="details">
@@ -34,8 +50,28 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                style={{ backgroundColor: theme }}
+                onClick={this.toggleModal}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="button">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No I am a monster</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 function Pet({ name, animal, breed, media, location, id }) {
   let hero = "http://placecorgi.com/300/300";
@@ -6,7 +7,7 @@ function Pet({ name, animal, breed, media, location, id }) {
     hero = media[0].small;
   }
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={hero.name} />
       </div>
@@ -14,7 +15,7 @@ function Pet({ name, animal, breed, media, location, id }) {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 }
 
